d23b42b0d93a0f8984309ba14cdceefca0e86448
arcarin <arcarin@sheridancollege.ca>
2021-12-30 13:12:21 -0500

    Imported shaders and hit visual effect. Attached environment shader to each object in test scene (Map_01)

---
+++ Assets/FX/Editor/EnviroMatEditor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EnviroMatEditor : ShaderGUI
+{
+    public override void OnGUI (MaterialEditor materialEditor, MaterialProperty[] properties) {
+
+        Material mat = materialEditor.target as Material;
+        //Material[] mats = materialEditor.targets as Material[];
+
+        EditorGUI.BeginChangeCheck();
+        bool shadowsOn = EditorGUILayout.Toggle("Shadows Enabled", mat.IsKeywordEnabled("SHADOWS_SCREEN"));
+
+        if (EditorGUI.EndChangeCheck()) {
+
+            //Material[] mats = materialEditor.targets as Material[];
+            if (shadowsOn == true) {
+                foreach (Material m in materialEditor.targets) {
+                    m.EnableKeyword("SHADOWS_SCREEN");
+                }
+            }
+            else {
+                foreach (Material m in materialEditor.targets) {
+                    m.DisableKeyword("SHADOWS_SCREEN");
+                }
+            }
+        }
+
+        base.OnGUI(materialEditor, properties);
+    }
+}
