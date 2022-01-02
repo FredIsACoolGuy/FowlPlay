ebe07a7fced8f7318e0984b1a192c9ab93562be8
arcarin <arcarin@sheridancollege.ca>
2022-01-02 14:13:43 -0500

    Added multiplier for char outline thickness

--- Assets/Scripts/OutlineColor.cs
+++ Assets/Scripts/OutlineColor.cs
@@ -7,10 +7,18 @@ public class OutlineColor : MonoBehaviour
     [HideInInspector] public Color outlineColor;
     [SerializeField] Renderer renderer;
     [SerializeField] bool isBird = false;
+    [SerializeField] float lineWeightMultiplier = 1;
 
     private void Awake() {
         if (isBird) {
             outlineColor = renderer.material.GetColor("_OutlineColor");
+            if (lineWeightMultiplier != 1) {
+                Material[] mats = renderer.materials;
+                float lineWeight = mats[0].GetFloat("_OutlineThickness");
+                foreach (Material mat in mats) {
+                    mat.SetFloat("_OutlineThickness", lineWeight * lineWeightMultiplier);
+                }
+            }
         }
         else {
             StartCoroutine(SetColor());
@@ -25,8 +33,11 @@ public class OutlineColor : MonoBehaviour
         if (renderer == null)
             renderer = GetComponent<Renderer>();
         Material[] mats = renderer.materials;
+
+        float lineWeight = mats[0].GetFloat("_OutlineThickness");
         foreach (Material mat in mats) {
             mat.SetColor("_OutlineColor", col);
+            mat.SetFloat("_OutlineThickness", lineWeight * lineWeightMultiplier);
         }
     }
 }
