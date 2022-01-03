64b1cd226466f83dee4f98ca4387c8e067862cc0
Zoe C. | Venn <khoyox@gmail.com>
2022-01-03 14:35:29 -0500

    Managers initialized and IPanel interface created

--- Assets/Scripts/DebuffMenu/PanelManager.cs
+++ Assets/Scripts/DebuffMenu/PanelManager.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class PanelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private List<GameObject> _panels;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
         
     }
